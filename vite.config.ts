d8df556f653ff0579438854398531800264ffcb8
natelwhite <natelwhite01@gmail.com>
2026-08-20 20:26:24 -0400

    testing

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,16 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit({
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '404.html',
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
+		}
+	})]
 });
